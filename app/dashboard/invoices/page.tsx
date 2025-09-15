554aaf0bb0a1020fb60b8db870c37f16b4277ebf
Aster <evan.sizty@gmail.com>
2025-09-15 11:15:21 +0800

    feat: init

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <>
+      <p>invoices page</p>
+    </>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Kode_Mono, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+export const kodeMono = Kode_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
